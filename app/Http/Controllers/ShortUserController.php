b48618211d33c0c0bdef2a7f83d8a6007bf6c54f
kiddo-xyz <hisyam.ali.crb@gmail.com>
2023-12-12 14:14:43 +0700

    Adding list of link

--- app/Http/Controllers/ShortUserController.php
+++ app/Http/Controllers/ShortUserController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ShortUserController extends Controller
 {
-    //
+    public function index(){
+        $links = auth()->user()->links;
+
+        return view('links.index', compact('links'));
+    }
 }
